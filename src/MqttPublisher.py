7ae9175f922d99ade23af18d4557ddf39ea31882
Ludwig <none@mail.com>
2024-12-23 15:40:44 +0100

    #3 fixed for mqtt client

--- src/MqttPublisher.py
+++ src/MqttPublisher.py
@@ -19,53 +19,45 @@ class MqttPublisher:
     def connect(self, client_id: str, username: str, password: str):
         self.client_id = client_id
 
+        self.unacked_publish = set()
+
         # For paho-mqtt 2.0.0, you need to add the properties parameter.
-        def on_connect(client, userdata, flags, rc, properties):
-            if rc == 0:
-                print("Connected to MQTT Broker!")
-                self.is_connected = True
+        def on_connect(client, userdata, flags, reason_code, properties):
+            print("Connected")
+            if reason_code.is_failure:
+                print(f"Failed to connect: {reason_code}. loop_forever() will retry connection")
             else:
-                print("Failed to connect, return code %d\n", rc)
-                self.is_connected = False
+                # we should always subscribe from on_connect callback to be sure
+                # our subscribed is persisted across reconnections.
+                client.subscribe("$SYS/#")
 
-        def on_disconnect(client, userdata, rc):
-
-            if self._graceful_disconnect:
-                return
-
-            print("Disconnected with result code: %s", rc)
-            reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
-            while reconnect_count < MAX_RECONNECT_COUNT:
-                print("Reconnecting in %d seconds...", reconnect_delay)
-                time.sleep(reconnect_delay)
-
-                try:
-                    client.reconnect()
-                    print("Reconnected successfully!")
-                    return
-                except Exception as err:
-                    print("%s. Reconnect failed. Retrying...", err)
-
-                reconnect_delay *= RECONNECT_RATE
-                reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
-                reconnect_count += 1
-            print("Reconnect failed after %s attempts. Exiting...", reconnect_count)
 
         # Set Connecting Client ID
         self.client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION2, client_id)
         # For paho-mqtt 2.0.0, you need to set callback_api_version.
         self.client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
+        self.client.user_data_set(self.unacked_publish)
         self.client.username_pw_set(username, password)
-        # self.client.on_connect = on_connect
-        # self.client.on_disconnect = on_disconnect
+        self.client.on_connect = on_connect
         self.client.connect(self.broker, self.port)
+
+        if self.client.is_connected():
+            print("Conn")
+        else:
+            print("Fail")
+
+        self.client.loop_start()
     
     def publish(self, payload: SensorData):
         result = self.client.publish(self.topic, payload)
+        self.unacked_publish.add(result.mid)
         status = result[0] # result: [0, 1]
         if status == 1:
             print(f"Failed to send message to topic {self.topic}")
+        elif status == 4:
+            print(f"No connection to broker to publish to topic {self.topic}")
 
     def disconnect(self):
         self._graceful_disconnect = True
         self.client.disconnect()
+        self.client.loop_stop()
